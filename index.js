39a43f350ae5d4290d2f01e962f6f1d144f30947
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-26 16:19:30 +0300

    create a div and add elements

--- index.js
+++ index.js
@@ -1,19 +1,111 @@
-// 
 
 const searchForm = document.querySelector("#form"); 
-searchForm.addEventListener("click", (event) => {
+const getStarted = document.getElementById("btn");
+const titleRecipe = document.getElementById("recipe-name");
+const category = document.getElementById("recipe-category")
+const image = document.getElementById("recipe-image");
+const instructions = document.getElementById("recipe-instructions");
+const mealImage = document.getElementById("meal-img")
+const mealName = document.getElementById("meal-name")
+const recipeCloseBtn = document.getElementById("recipe-close-btn")
+const recipe = document.getElementById("meal-recipes")
+const mealList = document.getElementById("meal")
+
+
+
+const mealRecipe = document.querySelector(".meal-recipes")
+searchForm.addEventListener("submit", (event) => {
     const searchButton = document.querySelector(".search-bar").value; 
     event.preventDefault();
     console.log(searchButton)
-})
-const food =`https://www.themealdb.com/api/json/v1/1/search.php?s=pizza`;
+    const food =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchButton}`;
     fetch(food)
     .then((res) => res.json())
     .then (dataRecipes => {
-        console.log(dataRecipes)
-    
-
+        mealRecipe.innerHTML = ``
+        displayMeal(dataRecipes)
     })
+    
+})
+
+function displayMeal(dataRecipes){
+    let mealArrays = dataRecipes.meals
+    //console.log(mealArrays)
+    mealArrays.forEach(mealObject => {
+        //console.log(mealObject)
+        console.log(mealObject.strMeal)
+        let mealName = mealObject.strMeal;
+        let mealImage = mealObject.strMealThumb;
+        let mealArea = mealObject.strArea
+        let mealInstructions = mealObject.strInstructions
+        let mealIngredient1 = mealObject.strIngredient1
+        let mealIngredient2 = mealObject.strIngredient2
+        let mealIngredient3 = mealObject.strIngredient3
+        let mealIngredient4 = mealObject.strIngredient4
+        let mealIngredient5 = mealObject.strIngredient5
+        let mealIngredient6 = mealObject.strIngredient6
+        let mealIngredient7 = mealObject.strIngredient7
+        let mealIngredient8 = mealObject.strIngredient8
+        let mealIngredient9 = mealObject.strIngredient9
+        let mealIngredient10 = mealObject.strIngredient10
+        let mealMeasure1 = mealObject.strMeasure1
+        let mealMeasure2 = mealObject.strMeasure2
+        let mealMeasure3 = mealObject.strMeasure3
+        let mealMeasure4 = mealObject.strMeasure4
+        let mealMeasure5 = mealObject.strMeasure5
+        let mealMeasure6 = mealObject.strMeasure6
+        let mealMeasure7 = mealObject.strMeasure7
+        let mealMeasure8 = mealObject.strMeasure8
+        let mealMeasure9 = mealObject.strMeasure9
+        let mealMeasure10 = mealObject.strMeasure10
+        //console.log(mealIngredient)
+        let displaydiv = document.createElement("div")
+        displaydiv.innerHTML = 
+        `<div class="mealItem">
+        <img src="${mealImage}" alt="${mealName}" id="meal-img"/>
+        <p class="paragraph">${mealName}</p>
+        <p class="paragraph" id="area">Country: ${mealArea}</p>
+        
+
+       
+        <button class="btn" id="getRecipe">Get Recipe</button>
+        <p id="likes">${likes} Likes </p>
+        <button class="btn" id="likeBtn">❤️</button>
+
+        <br>
+        <br>
+
+        <h3 class="h3instruction">Instructions:</h3>
+        <p class="paragraph ingredient-list" id="instruction">${mealInstructions}</p>
+        <h3 class="h3instruction">Ingredients:</h3>
+        <ol class="ingredient-list">
+        <li>${mealIngredient1}</li>
+        <li>${mealIngredient2}</li>
+        <li>${mealIngredient3}</li>
+        <li>${mealIngredient4}</li>
+        <li>${mealIngredient5}</li>
+        <li>${mealIngredient6}</li>
+        <li>${mealIngredient7}</li>
+        <li>${mealIngredient8}</li>
+        <li>${mealIngredient9}</li>
+        <li>${mealIngredient10}</li>
+        </ol>
+        <h3 class="h3instruction">Measurements:</h3>
+        <ol class="ingredient-list">
+        <li>${mealMeasure1}</li>
+        <li>${mealMeasure2}</li>
+        <li>${mealMeasure3}</li>
+        <li>${mealMeasure4}</li>
+        <li>${mealMeasure5}</li>
+        <li>${mealMeasure6}</li>
+        <li>${mealMeasure7}</li>
+        <li>${mealMeasure8}</li>
+        <li>${mealMeasure9}</li>
+        <li>${mealMeasure10}</li>
+        </ol>
+        </div>`
+    })
+}
 
 
 const form1 = document.getElementById("form1")
@@ -22,9 +114,9 @@ form1.addEventListener("click", (event)=>{
     event.preventDefault();
     console.log(searchInput);}) 
                 
-const food1 =`https://www.themealdb.com/api/json/v1/1/search.php?s=pizza`;
+const food =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
             
-fetch(food1)
+fetch(food)
 .then((res) => res.json())
 .then (dataRecipes => {
     console.log(dataRecipes)
@@ -32,19 +124,3 @@ fetch(food1)
             
     
             
-
-
-
-// searchForm.addEventListener("submit", (event) => {
-//     const searchButton = document.querySelector(".search-bar").value; 
-//     event.preventDefault();
-//     console.log(searchButton)
-//     const food =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchButton}`;
-//     fetch(food)
-//     .then((res) => res.json())
-//     .then (dataRecipes => {
-//         console.log(dataRecipes)
-            
-
-//     })
-// })
