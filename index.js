29878041597c7de132db230882b6bef57a044f11
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-26 15:43:30 +0300

    event listener click to search button

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// 
+
+const searchForm = document.querySelector("#form"); 
+searchForm.addEventListener("click", (event) => {
+    const searchButton = document.querySelector(".search-bar").value; 
+    event.preventDefault();
+    console.log(searchButton)
+})
 const food =`https://www.themealdb.com/api/json/v1/1/search.php?s=pizza`;
     fetch(food)
     .then((res) => res.json())
@@ -6,3 +14,37 @@ const food =`https://www.themealdb.com/api/json/v1/1/search.php?s=pizza`;
     
 
     })
+
+
+const form1 = document.getElementById("form1")
+form1.addEventListener("click", (event)=>{
+    const searchInput = document.getElementById("recipeSearch").value;
+    event.preventDefault();
+    console.log(searchInput);}) 
+                
+const food1 =`https://www.themealdb.com/api/json/v1/1/search.php?s=pizza`;
+            
+fetch(food1)
+.then((res) => res.json())
+.then (dataRecipes => {
+    console.log(dataRecipes)
+})
+            
+    
+            
+
+
+
+// searchForm.addEventListener("submit", (event) => {
+//     const searchButton = document.querySelector(".search-bar").value; 
+//     event.preventDefault();
+//     console.log(searchButton)
+//     const food =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchButton}`;
+//     fetch(food)
+//     .then((res) => res.json())
+//     .then (dataRecipes => {
+//         console.log(dataRecipes)
+            
+
+//     })
+// })
